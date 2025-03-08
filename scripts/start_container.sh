2c2fedc176cb55ea4d9ec416c4d69f0c49eac61f
Ramanjul Reddy Kotlo (Ram) <163696120+ramanjulreddykotlo@users.noreply.github.com>
2025-03-08 18:13:52 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push ramanjulreddy/python-flask-app
+docker pull ramanjulreddy/python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ramanjulreddy/python-flask-app
